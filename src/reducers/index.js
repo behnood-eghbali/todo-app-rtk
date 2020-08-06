0a7314ded7be92527e5b3db6c17e41b541f66ebd
behnood <behnoodii@gmail.com>
2020-08-06 17:08:26 +0430

    initial commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import todosReducer from './todosSlice'
+import todosReducer from '../todosSlice'
 
 export default combineReducers({
     todos: todosReducer
